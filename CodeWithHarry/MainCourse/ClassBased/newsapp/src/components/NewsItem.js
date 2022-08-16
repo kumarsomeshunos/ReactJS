db264b2d9448a28e74d7f78a327719457d5d6b5f
Somesh Kumar <kumarsomesh.unos@gmail.com>
2022-08-16 15:01:50 +0530

    add final touches

--- CodeWithHarry/MainCourse/ClassBased/newsapp/src/components/NewsItem.js
+++ CodeWithHarry/MainCourse/ClassBased/newsapp/src/components/NewsItem.js
@@ -33,7 +33,7 @@ export class NewsItem extends Component {
           </div>
         </a>
         <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-          {source.name}
+          {source}
         </span>
       </div>
     );
